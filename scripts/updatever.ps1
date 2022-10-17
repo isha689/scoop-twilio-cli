7425f15c99199896e3fd725eb579608ba635b4eb
Isha Bansal <ishabansal2467@gmail.com>
2022-10-17 21:51:00 +0530

    fix: draft update manifest

--- scripts/updatever.ps1
+++ scripts/updatever.ps1
@@ -4,4 +4,8 @@ param(
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$psscriptroot/../" # checks the parent dir
-Invoke-Expression -command "& '$checkver' -a '$psscriptroot/../twiliodraft.json' -Update -v '$version'"
+if($version -like "*draft*"){
+    Invoke-Expression -command "& '$checkver' -a '$psscriptroot/../twiliodraft.json' -Update -v '$version'"
+}else{
+    Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " }) -Update"
+}
